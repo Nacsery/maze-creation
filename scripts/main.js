009704b612532084a37498f13d1f0dd92a327482
Nacsery <hp_bs@outlook.com>
2021-03-11 16:34:17 +0300

    study2

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,58 @@
-const Http = new XMLHttpRequest()
+const header = document.querySelector('header');
+const section = document.querySelector('section');
 
-let streamRequest = (__url__, preload = 'none', autoplay = 'false', width = '1280', height = '720') =>{
-    let url =`https://web.microsoftstream.com/oembed?url=${__url__}&preload=${preload}&autoplay=${autoplay}&width=${width}&height=${height}`
-    console.log(url);
-    return url;
+let requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
+let request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+request.onload = function () {
+  const superHeroes = request.response;
+  populateHeader(superHeroes);
+  showHeroes(superHeroes);
 }
 
-fetch(streamRequest('https://web.microsoftstream.com/video/a22a6040-3011-4c8e-a49e-6f45466f0eba'),{mode: 'cors'})
-.then(response => response.json())
-.then(data => console.log(data));
+function populateHeader(obj) {
+  console.log("asda");
+  const myH1 = document.createElement('h1');
+  myH1.textContent = obj['squadName'];
+  header.appendChild(myH1);
 
+  const myPara = document.createElement('p');
+  myPara.textContent = 'Hometown: ' + obj['homeTown'] + ' // Formed: ' + obj['formed'];
+  header.appendChild(myPara);
+}
+
+function showHeroes(obj) {
+  const heroes = obj['members'];
+  console.log("asda");
+  for (let i = 0; i < heroes.length; i++) {
+    const myArticle = document.createElement('article');
+    const myH2 = document.createElement('h2');
+    const myPara1 = document.createElement('p');
+    const myPara2 = document.createElement('p');
+    const myPara3 = document.createElement('p');
+    const myList = document.createElement('ul');
+
+    myH2.textContent = heroes[i].name;
+    myPara1.textContent = 'Secret identity: ' + heroes[i].secretIdentity;
+    myPara2.textContent = 'Age: ' + heroes[i].age;
+    myPara3.textContent = 'Superpowers:';
+
+    const superPowers = heroes[i].powers;
+    for (let j = 0; j < superPowers.length; j++) {
+      const listItem = document.createElement('li');
+      listItem.textContent = superPowers[j];
+      myList.appendChild(listItem);
+    }
+
+    myArticle.appendChild(myH2);
+    myArticle.appendChild(myPara1);
+    myArticle.appendChild(myPara2);
+    myArticle.appendChild(myPara3);
+    myArticle.appendChild(myList);
+
+    section.appendChild(myArticle);
+  }
+}
