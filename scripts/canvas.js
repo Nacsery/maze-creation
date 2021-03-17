1f6b7157591489063421aaf25bbb7b6a9843e781
Nacsery <hp_bs@outlook.com>
2021-03-17 23:09:13 +0300

    new comments

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,17 +1,26 @@
+//Author Burak Keser
 const canvas = document.querySelector('canvas');
+//canvas
 const ctx = canvas.getContext('2d');
-const bg_colour = window.getComputedStyle(document.querySelector('body')).backgroundColor
-const width = canvas.width = 1920;
-const height = canvas.height = width*9/16;
-const mazeRow = (height / 30) - 1;
-const mazeColumn = (width / 30) - 1;
-let maze = [];
-let movingBlock = new Block(0, 0, 'black', 30);
-function random(min, max) {
-    const num = Math.floor(Math.random() * (max - min + 1)) + min;
-    return num;
-}
 
+//resolution
+const width = canvas.width = 1920;
+const height = canvas.height = 9 * width / 16;
+
+//size of nodes
+let size = 0.015625 * width;
+
+//row and column numbers for the array containing the maze
+const mazeRow = (height / size) - 1;
+const mazeColumn = (width / size) - 1;
+
+//maze array
+let maze = [];
+
+//moving red block
+let movingBlock = new Block(0, 0, 'black', size);
+
+//Block object for the grid
 function Block(x, y, color, size) {
 
     this.x = x;
@@ -20,7 +29,7 @@ function Block(x, y, color, size) {
     this.size = size;
     this.visited = false;
 }
-
+//Draws an empty square for grid
 Block.prototype.draw = function () {
     ctx.beginPath();
     ctx.fillStyle = this.color;
@@ -31,31 +40,35 @@ Block.prototype.draw = function () {
     ctx.rect(this.x + 1, this.y + 1, this.size - 2, this.size - 2);
     ctx.fill();
 }
+//Deletes the inner square
 Block.prototype.deleteInner = function () {
     ctx.beginPath();
     ctx.fillStyle = 'white';
     ctx.rect(this.x + 1, this.y + 1, this.size - 2, this.size - 2);
     ctx.fill();
 }
+//Draws an inner square
+//used by moving node
 Block.prototype.drawInner = function () {
     ctx.beginPath();
     ctx.fillStyle = 'red';
-    ctx.rect(this.x + 2, this.y + 2, this.size - 4, this.size - 4);
+    ctx.rect(this.x + 1, this.y + 1, this.size - 2, this.size - 2);
     ctx.fill();
 }
-
+//Deletion of the node from Screen
 Block.prototype.delete = function () {
     ctx.beginPath();
     ctx.fillStyle = 'white';
     ctx.rect(this.x, this.y, this.size, this.size);
     ctx.fill();
 }
-
+//Updates the position of the block
+//Used by moving block
 Block.prototype.update = function (x, y) {
     this.x = x;
     this.y = y;
 }
-
+//Removes the Wall from given direction
 Block.prototype.removeWall = function (direction) {
     ctx.beginPath();
     ctx.fillStyle = 'white';
@@ -78,25 +91,31 @@ Block.prototype.removeWall = function (direction) {
     ctx.fill();
 }
 
+//Finds the unvisited neighbor of the given node
 function unvisitedNeighbors(x, y) {
     let unvisitedNeighbors = [];
+    //Before the rightmost node and not visited before
     if (x < mazeColumn && !maze[y][x + 1].visited) {
         unvisitedNeighbors.push([x + 1, y, 'right']);
     }
+    //Before the lowermost node and not visited before
     if (y < mazeRow && !maze[y + 1][x].visited) {
         unvisitedNeighbors.push([x, y + 1, 'down']);
     }
+    //before the leftmost node and not visited before
     if (x > 0 && !maze[y][x - 1].visited) {
         unvisitedNeighbors.push([x - 1, y, 'left']);
     }
+    //before the upmost node and not visited before
     if (y > 0 && !maze[y - 1][x].visited) {
         unvisitedNeighbors.push([x, y - 1, 'up']);
     }
     return unvisitedNeighbors;
 }
 
+//Creates a grid for maze creation
 function createGrid() {
-    let size = 30;
+
     let block;
     for (let i = 0; i < height; i += size) {
         let row = []
@@ -108,28 +127,38 @@ function createGrid() {
         maze.push(row);
     }
 }
+
+//Promise for the animation
 const timer = ms => new Promise(res => setTimeout(res, ms))
+
+//Returns random neighbor from unvisited neighbor array
 async function randomUnvisitedNeighbor(x, y) {
     let neighbors = unvisitedNeighbors(x, y);
     let randomNeighbor = neighbors[Math.floor(Math.random() * neighbors.length)];
     return randomNeighbor;
 }
 
+//Randomized Depth First Search algorithm
+//Recursively go down on a random path on the nodes
+//After encountering a node with surrounding nodes visited go up on the recursion
+//While travelling unblock the wall between the nodes
+//displays the current node with moving red block
 async function randomizedDFS(x, y) {
     maze[y][x].visited = true;
     let nextBlock = await randomUnvisitedNeighbor(x, y);
     while (nextBlock != undefined) {
         await movingBlock.deleteInner();
-        
-        movingBlock.update(x*30,y*30);
+        movingBlock.update(x * size, y * size);
         movingBlock.drawInner();
-        await timer(20);
+        await timer(0.5);
         await maze[y][x].removeWall(nextBlock[2]);
         await randomizedDFS(nextBlock[0], nextBlock[1]);
         nextBlock = await randomUnvisitedNeighbor(x, y);
     }
+    movingBlock.deleteInner();
 }
 
+//wrapper function for maze creation
 function createMaze() {
     randomizedDFS(0, 0);
 }
