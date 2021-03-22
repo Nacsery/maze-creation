32d8d22b17a6b7c80fa6225da99e5f5e93272175
Nacsery <hp_bs@outlook.com>
2021-03-22 18:29:07 +0300

    coded multi block grid for pathfinding

--- scripts/grid-handler.js
+++ scripts/grid-handler.js
@@ -2,6 +2,7 @@
 
 import { GridMaker } from './modules/grid-maker.js';
 import { MazeCreator } from './modules/maze-creator.js';
+import { MultiBlockGrid } from './modules/multi-block-grid.js';
 
 let canvas = document.querySelector('canvas');
 let ctx = canvas.getContext('2d');
@@ -9,21 +10,19 @@ let ctx = canvas.getContext('2d');
 let gridMaker = new GridMaker(30);
 let mazeCreator = new MazeCreator();
 let grid = gridMaker.newGrid();
-
+let multiBlockGrid = new MultiBlockGrid(gridMaker.gridRow, gridMaker.girdColumn,4);
+let buttonColour = document.getElementById('multi-block').style.background;
 //gets size range element
 document.getElementById('current-height').innerHTML = gridMaker.heightFactor;
 let heightInput = document.getElementById('height');
 
 //changes the size of blocks and string near the range input of size
 heightInput.oninput = () => {
-
     gridMaker.height = canvas.height = Number(document.getElementById('height').value) * gridMaker.blockSize;
     gridMaker.gridRow = (gridMaker.height / gridMaker.blockSize) - 1
     document.getElementById('current-height').innerHTML = document.getElementById('height').value;
-    gridMaker.newGrid();
     reset();
 }
-
 
 //gets size range element
 document.getElementById('current-width').innerHTML = gridMaker.widthFactor;
@@ -34,10 +33,54 @@ widthInput.oninput = () => {
     gridMaker.width = canvas.width = Number(document.getElementById('width').value) * gridMaker.blockSize;
     gridMaker.girdColumn = (gridMaker.width / gridMaker.blockSize) - 1
     document.getElementById('current-width').innerHTML = document.getElementById('width').value;
-    gridMaker.newGrid();
     reset();
+}
 
 
+let multiBlockSize = 3;
+document.getElementById('current-block-size').innerHTML = multiBlockSize;
+let multiBlockSizeInput = document.getElementById('block-size');
+
+//changes the size of blocks and string near the range input of size
+multiBlockSizeInput.oninput = () => {
+    multiBlockSize = Number(document.getElementById('block-size').value);
+    document.getElementById('current-block-size').innerHTML = document.getElementById('block-size').value;
+    reset();
+}
+
+
+let multiGrid = true
+let multiGridInput = document.getElementById('multi-block');
+
+//start and stop functionality for maze
+multiGridInput.onclick = async () => {
+    if(firstStart){
+        if (multiGrid == true) {
+            multiGrid = false;
+            let sizeBlock = multiBlockSize-1;
+            let roundedHeight = (gridMaker.gridRow-1) +  Math.abs(((gridMaker.gridRow-1) % sizeBlock) - sizeBlock) + 1;
+            let roundedWidth = (gridMaker.girdColumn-1) +  Math.abs(((gridMaker.girdColumn-1) % sizeBlock) - sizeBlock) + 1;
+    
+            gridMaker.width = canvas.width = roundedWidth * gridMaker.blockSize;
+            gridMaker.girdColumn = (gridMaker.width / gridMaker.blockSize) - 1
+            document.getElementById('current-width').innerHTML = roundedWidth;
+            gridMaker.newGrid();
+    
+            gridMaker.height = canvas.height =roundedHeight * gridMaker.blockSize;
+            gridMaker.gridRow = (gridMaker.height / gridMaker.blockSize) - 1
+            document.getElementById('current-height').innerHTML = roundedHeight;
+            gridMaker.newGrid();
+    
+            grid = gridMaker.newGrid();
+            multiBlockGrid.reset(gridMaker.gridRow,gridMaker.girdColumn,multiBlockSize)
+    
+            multiBlockGrid.newMultiBlockGrid(grid);
+            multiGridInput.value = 'Clear Grid';
+    
+        } else {
+            reset();
+        }
+    }
 }
 
 //speed of iteration
@@ -56,17 +99,19 @@ let executionStatus = false;
 
 //start and stop functionality for maze
 stopStart.onclick = async () => {
+    console.log( document.getElementById('multi-block').style.background );
     if (firstStart) {
         firstStart = false;
         mazeCreator.start();
         stopStart.value = 'Stop';
-        let grid = gridMaker.newGrid();
+        grid = gridMaker.newGrid();
         mazeCreator.reset = false;
+        document.getElementById('multi-block').style.background = 'grey';
         await mazeCreator.createMaze(grid, gridMaker.blockSize);
 
         if (mazeCreator.isReset) {
             mazeCreator.isReset = false;
-            gridMaker.newGrid();
+            grid = gridMaker.newGrid();
         }
         mazeCreator.stop();
         stopStart.value = 'Start';
@@ -89,10 +134,13 @@ stopStart.onclick = async () => {
 const reset = async () => {
     mazeCreator.restart();
     mazeCreator.start();
+    multiGrid = true;
+    multiGridInput.value = 'Block Grid';
     stopStart.value = 'Start';
     firstStart = true
     executionStatus = false;
-    gridMaker.newGrid();
+    grid = gridMaker.newGrid();
+    document.getElementById('multi-block').style.background = buttonColour;
 }
 
 //Reset functionality
--- scripts/modules/multi-block-grid.js
+++ scripts/modules/multi-block-grid.js
@@ -1,50 +1,33 @@
 //Author Burak Keser
-import { gridRow, girdColumn, newGrid } from './grid-maker.js';
 import { MultiBlock } from './multiple-block.js';
 
-//Multi Block size, represents edge size of multi block
-export let multiBlockSize = 4;
 
-let multiBlockGridRow = gridRow / multiBlockSize - 1;
-let multiBlockGridColumn = girdColumn / multiBlockSize - 1;
-
-let multiGrid = true
-let multiGridInput = document.getElementById('multi-block');
-
-
-//start and stop functionality for maze
-multiGridInput.onclick = () => {
-
-    if (multiGrid == true) {
-        multiGrid = false;
-        newMultiBlockGrid();
-        multiGridInput.value = 'Clear Block Grid';
-
-    } else {
-        multiGrid = true;
-        newGrid();
-        multiGridInput.value = 'Block Grid';
-    }
-
+export function MultiBlockGrid(gridRow, girdColumn, size) {
+    this.multiBlockSize = size;
+    this.gridRow = gridRow;
+    this.girdColumn = girdColumn;
+    this.multiBlockGrid = [];
 }
 
-
-
-//Multi block grid
-export let multiBlockGrid = [];
-
-
-export function newMultiBlockGrid(grid) {
-    multiBlockGrid = [];
-    for (let i = 0; i < girdColumn; i += multiBlockSize - 1) {
+MultiBlockGrid.prototype.newMultiBlockGrid = function (grid) {
+    this.multiBlockGrid = [];
+    for (let i = 0; i < this.girdColumn; i += this.multiBlockSize - 1) {
         let row = []
-        for (let j = 0; j < gridRow - 1; j += multiBlockSize - 1) {
-            let block = new MultiBlock(j, i, multiBlockSize);
+        
+        for (let j = 0; j < this.gridRow - 1; j += this.multiBlockSize - 1) {
+            let block = new MultiBlock(j, i, this.multiBlockSize);
             block.structInnerGrid(grid);
             row.push(block);
             block.draw();
         }
-        multiBlockGrid.push(row);
+        this.multiBlockGrid.push(row);
     }
+}
+
+MultiBlockGrid.prototype.reset = function (gridRow, girdColumn, size) {
+    this.gridRow = gridRow;
+    this.girdColumn = girdColumn;
+    this.multiBlockSize = size;
+    this.multiBlockGrid = [];
 }
 
